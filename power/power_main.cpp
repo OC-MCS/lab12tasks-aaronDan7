fbc57046f6fe8e0b2c00ee4c22ae35ba1490c49c
Aaron Hill <aaron.hill@eagles.oc.edu>
2019-04-17 12:42:40 -0500

    working

--- power/power_main.cpp
+++ power/power_main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int calculatePower(int, int);
 
 int main()
 {
+	int n = 2, exp = 4, total;
 
+	total = calculatePower(n, exp);
+	cout << n << "^" << exp << " = " << total;
 
 	return 0;
+}
+
+int calculatePower(int n, int exp)
+{
+	if (exp != 0)
+		return (n*calculatePower(n, exp - 1));
+	else
+		return 1;
 }
--- sign/sign_main.cpp
+++ sign/sign_main.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 void sign(int n)
 {
-	while (n > 0)
-	{
 		cout << "No Parking: " << n << endl;
 		n--;
-	}
+		if (n > 0)
+		sign(n);
 } 
 
 int main()
 {
 	sign(4);
+
 	return 0;
 }
 
